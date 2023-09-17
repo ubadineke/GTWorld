c9824afa26bdbbbb4ec06f68eb542333be4fa0ed
ubadineke <ubadinekeprince@gmail.com>
2023-09-17 06:29:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var zoom = document.querySelectorAll(".btnn");
+zoom.addEventListener('click', clintbarton);
+console.log(zoom);
+
+function clintbarton (e){
+    document.querySelector(".signin").style.color="#000";
+}
+
